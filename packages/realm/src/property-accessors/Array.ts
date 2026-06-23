c36c69c510e94996a20c6ce4f38b2bec9d1f2bbe
非为 <nothing.feiwei@gmail.com>
2026-06-23 20:12:30 +0800

    Revert "feat:升级realm core到14.14.0，适配autolink，realm-js升级到12.15.0版本 (#28)" (#34)
    This reverts commit 5deae4b65efe3442b969fcdd7dc845bbadee2faa.

--- packages/realm/src/property-accessors/Array.ts
+++ packages/realm/src/property-accessors/Array.ts
@@ -87,13 +87,11 @@ export function createArrayPropertyAccessor({
         assert.inTransaction(realm);
         assert.iterable(values);
 
-        // Taking a snapshot in case we're iterating the list we're mutating
-        const valuesSnapshot = values instanceof List ? values.snapshot() : values;
         const internal = binding.List.make(realm.internal, obj, columnKey);
         internal.removeAll();
         let index = 0;
         try {
-          for (const value of valuesSnapshot) {
+          for (const value of values) {
             listAccessor.insert(internal, index++, value);
           }
         } catch (err) {
